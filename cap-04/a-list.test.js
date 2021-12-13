77895c41b765f9956a76e694f36fc78732d0342c
viviane.miranda <viviane.miranda@nubank.com.br>
2021-12-13 11:28:15 -0300

    adding eslint and editorconfig to the project

--- cap-04/a-list.test.js
+++ cap-04/a-list.test.js
@@ -1,34 +1,34 @@
-function arrayToList(arr){
+function arrayToList(arr) {
   console.log(arr);
   let list = null;
-  for(i=arr.length-1; i>=0; i--){
-      list = {
-          value: arr[i],
-          rest: list
-        }
+  /* eslint no-plusplus: ["error", { "allowForLoopAfterthoughts": true }] */
+  for (let i = arr.length - 1; i >= 0; i--) {
+    list = {
+      value: arr[i],
+      rest: list,
+    };
   }
   return list;
-};      
+}
 
-
-describe("A list exercise", ()=>{
-    it("must convert [1,2,3] array to a list object", ()=>{
-        expect(arrayToList([1,2,3])).toEqual({
-            value: 1,
-            rest: {
-              value: 2,
-              rest: {
-                value: 3,
-                rest: null
-              }
-            }
-          });
+describe('A list exercise', () => {
+  it('must convert [1,2,3] array to a list object', () => {
+    expect(arrayToList([1, 2, 3])).toEqual({
+      value: 1,
+      rest: {
+        value: 2,
+        rest: {
+          value: 3,
+          rest: null,
+        },
+      },
     });
+  });
 
-    it("must convert [50] array to a list object", ()=>{
-        expect(arrayToList([50])).toEqual({
-            value: 50,
-            rest: null
-        }); 
+  it('must convert [50] array to a list object', () => {
+    expect(arrayToList([50])).toEqual({
+      value: 50,
+      rest: null,
     });
+  });
 });
